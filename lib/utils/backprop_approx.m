61f5785ff1a6194ae6c7dd1829b0e7671cd5d356
josephmcg16 <joseph.mcgovern16@googlemail.com>
2022-05-13 18:27:32 +0100

    Delete backprop function
    Built-in to the 'nnCostFunction.m' function now instead.

--- lib/utils/backprop_approx.m
+++ lib/utils/backprop_approx.m
@@ -1,10 +1,7 @@
-function gradJ_diff = backprop_approx(cost_func, Weights, delta)
+function gradJ_diff = backprop_approx(cost_func, Weights, dWeights)
 
 gradJ_diff = zeros(length(Weights), 1);
 for i = 1:length(Weights)
-    dWeights = zeros(length(Weights), 1);
-    dWeights(i) = delta;
-
     dJ = cost_func(Weights + dWeights) - cost_func(Weights);
     gradJ_diff(i) = dJ / delta;
 end
